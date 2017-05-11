98e25941273677b445ca5a924d1941c70e05630f
YauheniKhilabok <khilobok95@gmail.com>
2017-05-11 16:40:16 +0300

    add cross origin support to the backend

--- src/main/java/com/epam/invpol/controller/ProgramController.java
+++ src/main/java/com/epam/invpol/controller/ProgramController.java
@@ -18,10 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/programs")
 public class ProgramController {
 
--- src/main/java/com/epam/invpol/controller/UserController.java
+++ src/main/java/com/epam/invpol/controller/UserController.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/users")
 public class UserController {
 
